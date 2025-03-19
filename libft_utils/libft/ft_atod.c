53d90f2276ae98427ad08f58d588ee05bbb956ee
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-03-19 15:28:40 +0000

    v15

--- libft_utils/libft/ft_atod.c
+++ libft_utils/libft/ft_atod.c
@@ -6,50 +6,106 @@
 /*   By: mourhouc <mourhouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 15:03:33 by mourhouc          #+#    #+#             */
-/*   Updated: 2025/03/18 09:13:55 by mourhouc         ###   ########.fr       */
+/*   Updated: 2025/03/19 15:26:35 by mourhouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "../printf/ft_printf.h"
 
-double	ft_atoi_part(const char *str, int *i)
+double ft_atoi_part(const char **str)
 {
-	double	res;
+    double res;
 
 	res = 0.0;
-	while (str[*i] >= '0' && str[*i] <= '9')
-	{
-		res = res * 10 + (str[*i] - '0');
-		(*i)++;
-	}
-	return (res);
+    while (**str >= '0' && **str <= '9')
+    {
+        res = res * 10 + (**str - '0');
+        (*str)++;
+    }
+    return res;
 }
+
+// double	ft_atod(const char *str)
+// {
+// 	double (res), (sign), (decimal_place);
+// 	res = ((sign = 1.0, decimal_place = 1.0), 0.0);
+// 	while (*str == ' ' || (*str >= 9 && *str <= 13))
+// 		str++;
+// 	if (*str == '-' || *str == '+')
+// 	{
+// 		if (*str == '-')
+// 			sign = -1.0;
+// 		str++;
+// 	}
+// 	res = ft_atoi_part(&str);
+// 	if (*str == '.')
+// 	{
+// 		str++;
+// 		while (*str >= '0' && *str <= '9')
+// 		{
+// 			decimal_place *= 0.1;
+// 			res += (*str - '0') * decimal_place;
+// 			str++;
+// 		}
+// 		if (*str)
+// 			exit(EXIT_FAILURE);
+// 	}
+// 	return (res * sign);
+// }
+
+// double	ft_atoi_part(const char **str)
+// {
+//     double	res;
+// 	double	sign;
+
+// 	sign = 1.0;
+// 	res = 0.0;
+// 	if (**str == '-' || **str == '+')
+//     {
+//         if (**str == '-')
+//             sign = -1.0;
+//         (*str)++;
+//     }
+//     while (**str >= '0' && **str <= '9')
+//     {
+//         res = res * 10 + (**str - '0');
+//         (*str)++;
+// 		// if ((res > 2147483647.0 && sign == 1.0) || (res > 2147483648.0 && sign == -1.0))
+// 		// {
+// 		// 	ft_printf("Error: Number out of range!");
+// 		// 	exit(EXIT_FAILURE);
+// 		// }
+// 		if (**str != '.')
+//         	exit(EXIT_FAILURE);
+//     }
+//     return (res * sign);
+// }
 
 double	ft_atod(const char *str)
 {
-	int	i;
-
-	double (res), (sign), (decimal_place);
+    double (res), (sign), (decimal_place);
 	res = ((sign = 1.0, decimal_place = 1.0), 0.0);
-	i = 0;
-	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-		i++;
-	if (str[i] == '-' || str[i] == '+')
+    while (*str == ' ' || (*str >= 9 && *str <= 13))
+        str++;
+	if (*str == '-' || *str == '+')
 	{
-		if (str[i] == '-')
+		if (*str == '-')
 			sign = -1.0;
-		i++;
+		str++;
 	}
-	res = ft_atoi_part(str, &i);
-	if (str[i] == '.')
-	{
-		i++;
-		while (str[i] >= '0' && str[i] <= '9')
-		{
-			decimal_place *= 0.1;
-			res += (str[i] - '0') * decimal_place;
-			i++;
-		}
-	}
-	return (res * sign);
+    res = ft_atoi_part(&str);
+    if (*str == '.')
+    {
+		str++;
+        while (*str >= '0' && *str <= '9')
+        {
+            decimal_place *= 0.1;
+            res += (*str - '0') * decimal_place;
+            str++;
+        }
+    }
+    if (*str || res > 2147483647.0 || res < -2147483648.0)
+        exit(EXIT_FAILURE);
+    return (res * sign);
 }
--- libft_utils/libft/libft.h
+++ libft_utils/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mourhouc <mourhouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 16:47:13 by mourhouc          #+#    #+#             */
-/*   Updated: 2025/03/18 09:14:53 by mourhouc         ###   ########.fr       */
+/*   Updated: 2025/03/19 12:10:25 by mourhouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-size_t				ft_strlen(const char *s);
-int					ft_strncmp(const char *s1, const char *s2, size_t n);
-double				ft_atod(const char *str);
+size_t	ft_strlen(const char *s);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+double	ft_atoi_part(const char **str);
+double	ft_atod(const char *str);
 
 #endif
