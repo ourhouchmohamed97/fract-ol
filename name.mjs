c278cf378100f9b441804b5edc6d4b0ec23a8f42
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-03-20 14:28:26 +0000

    fractol

--- name.mjs
+++ name.mjs
@@ -1,21 +1,15 @@
 import figlet from 'figlet';
-import chalk from 'chalk'; // Optional, only for coloring
+import chalk from 'chalk';
 
-// Clear console
 console.clear();
 
-// Generate ASCII text
 figlet.text('FRACTOL', { font: 'Big' }, function (err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
         return;
     }
-
-    // Apply a solid color directly (cyan in this case)
-    const solidColorText = chalk.cyan.bold(data);  // Change 'cyan' to any other color if needed
-
-    // Display the solid color text
+    const solidColorText = chalk.cyan.bold(data); 
     console.log(solidColorText);
 });
 
